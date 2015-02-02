4c1dc0f4bc97cf0f3bf7d5b8ccef278a21276244
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2015-02-02 00:06:34 -0700

    Better centering the initials.

--- app/avatar.js
+++ app/avatar.js
@@ -6,14 +6,12 @@ module.exports = function toPng (initials, backgroundColor, foregroundColor, opt
     height: options.height || 200,
     fontWeight: options.fontWeight || 100,
     fontSize: options.fontSize || null,
-    fontFamily: options.fontFamily || "'Lato', 'Lato-Regular', 'Helvetica Neue'",
+    fontFamily: options.fontFamily || "'LatoHairline', 'LatoRegular', 'HelveticaNeue'",
     density: options.density || 1
   };
 
   var width = options.width;
   var height = options.height;
-  var x = width / 2;
-  var y = height / 2;
   var fontSize = options.fontSize || height / 2;
 
   var canvas = new Canvas(width * options.density, height * options.density);
@@ -23,9 +21,11 @@ module.exports = function toPng (initials, backgroundColor, foregroundColor, opt
   context.fillStyle = backgroundColor;
   context.fill();
   context.font = '' + options.fontWeight + ' ' + fontSize + 'px ' + options.fontFamily;
-  context.textAlign = 'center';
   context.textBaseline = 'middle';
   context.fillStyle = foregroundColor;
+
+  var x = (width / 2) - (context.measureText(initials).width / 2)
+  var y = height / 2;
   context.fillText(initials, x, y);
   return canvas.pngStream();
 };
