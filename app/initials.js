04d86a9509a784fa211dae566e14ae96874ea1d2
Nicholas Pufal <nicholas@petrofeed.com>
2015-08-07 11:27:25 -0300

    Fix: should show only first and last name as initials

--- app/initials.js
+++ app/initials.js
@@ -1,5 +1,11 @@
-module.exports = function initials (name) {
-  return name.split(' ')
+module.exports = function initials (fullName) {
+  function firstAndLastNames(name) {
+    var names = name.split(' ');
+    names.splice(1, names.length-2);
+    return names;
+  }
+
+  return firstAndLastNames(fullName)
     .map(function (word) { return word.charAt(0); })
     .join('')
     .toUpperCase();
